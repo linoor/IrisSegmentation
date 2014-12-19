cea6c9b8ce77792d601e34f3a473e62283c42161
Michał Pomarański <michal.pomaranski@gmail.com>
2014-12-19 18:08:53 +0100

    binarisation

--- project.py
+++ project.py
@@ -4,12 +4,17 @@ import smilPython as smil
 images_folder = r"E:\Dropbox\Francja\Erasmus 2014\Mines ParisTech\Analyse d'images\examen_ES14\UBIRIS"
 
 def get_all_images():
-	return [Image(os.path.join(images_folder, img_path)) for img_path in os.listdir(images_folder)]
+    return [Image(os.path.join(images_folder, img_path)) for img_path in os.listdir(images_folder)]
 
 def copy_image(im):
-	imOut = Image(im)
-	smil.copy(im, imOut)
-	return imOut
+    imOut = Image(im)
+    smil.copy(im, imOut)
+    return imOut
+
+def binarise(im):
+    imOut = copy_image(im)
+    threshold(im, 0, 100, imOut)
+    return imOut
 
 def extract_reflections(im):
     imOut = copy_image(im)
@@ -19,21 +24,21 @@ def extract_reflections(im):
     return imOut
 
 def diff(im, im2):
-	imOut = copy_image(im)
-	compare(im, "!=", im2, 255, 0, imOut)
-	return imOut
+    imOut = copy_image(im)
+    compare(im, "!=", im2, 255, 0, imOut)
+    return imOut
 
 def main():
-	# choosing an image
-	image_name = "I13.png"
-	original_image = Image(os.path.join(images_folder, image_name))
-	original_image.show()
+    # choosing an image
+    image_name = "I13.png"
+    original_image = Image(os.path.join(images_folder, image_name))
+    original_image.show()
 
-	# reflection extraction
-	no_reflections = extract_reflections(original_image)
-	no_reflections.show()
+    # binarisation
+    binarised_img = binarise(original_image)
+    binarised_img.show()
 
-	map(lambda img: extract_reflections(img).show(), get_all_images())
+    # map(lambda img: extract_reflections(img).show(), get_all_images())
 
 if __name__ == "__main__":
-	main()
+    main()

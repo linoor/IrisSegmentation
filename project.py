20f19e3ba2aa6b86df5c91b7ccd798751bb61937
Michał Pomarański <michal.pomaranski@gmail.com>
2014-12-31 00:50:07 +0100

    ready to send

--- project.py
+++ project.py
@@ -60,6 +60,9 @@ def calculate_circle(binary_im):
                 x_sum += i
                 y_sum += j
 
+    if num_black_pixels == 0:
+        print("the eye is closed")
+
     result_x = int((1.0/num_black_pixels) * x_sum)
     result_y = int((1.0/num_black_pixels) * y_sum)
     result_r = int(math.sqrt(num_black_pixels / math.pi))
@@ -76,19 +79,22 @@ def main():
     no_reflections = copy_image(original_image)
     closed_pupil = Image(original_image)
     result = Image(original_image)
+    write(original_image, "original.png")
 
     # normalizing the image
     im = normalize(original_image)
+    write(im, "normalized.png")
     # im.show()
 
     # removing reflections over the area of the pupil
-    no_reflections.getPixel(76, 50)
     for i in range(30, 100):
         for j in range(25, 90):
             if no_reflections.getPixel(i, j) > 240:
                 no_reflections.setPixel(i, j, 0)
     compare(no_reflections, "==", 0, no_reflections, im, no_reflections)
+    write(no_reflections, "no_reflections_darkened.png")
     smil.open(no_reflections, no_reflections, HexSE(2))
+    write(no_reflections, "no_reflections_opened.png")
     # no_reflections.show()
 
     # thresholding image to get the pupil (sometimes we get also the eyelashes)
@@ -98,34 +104,43 @@ def main():
 
     # removing eyelashes, closing the pupil
     smil.close(binarised_img, closed_pupil, VertSE(2))
+    write(closed_pupil, "no_eyelashes.png")
     smil.open(closed_pupil, closed_pupil, HexSE(9))
+    write(closed_pupil, "closed_pupil.png")
     # closed_pupil.show()
 
     # calculating the the pupil's center of mass from the binary image
     pupil_x, pupil_y, pupil_r = calculate_circle(closed_pupil)
 
     # showing where the pupil is
-    pupil_showed = Image(original_image)
-    pupil_showed << 0
+    pupil_showed = copy_image(original_image)
     draw_circle(pupil_showed, pupil_x, pupil_y, pupil_r)
+    write(pupil_showed, "closed_pupil_original.png")
     # pupil_showed.show()
 
-    # gradient
-    grad = copy_image(no_reflections)
-    close(grad, grad, SquSE(20))
-    threshold(grad, grad)
-    grad.show()
+    # threshold of iris
+    iris_threshold = copy_image(no_reflections)
+
+    iris_avant_close = copy_image(no_reflections)
+    threshold(iris_avant_close, iris_avant_close)
+    write(iris_avant_close, "iris_avant_close.png")
+
+    close(iris_threshold, iris_threshold, SquSE(10))
+    threshold(iris_threshold, iris_threshold)
+    write(iris_threshold, "iris_threshold.png")
+    iris_threshold.show()
 
     # calculating iris radius and center
-    iris_x, iris_y, iris_r = calculate_circle(grad)
-    iris_showed = Image(original_image)
-    iris_showed << 0
+    iris_x, iris_y, iris_r = calculate_circle(iris_threshold)
+    iris_showed = copy_image(original_image)
     draw_circle(iris_showed, iris_x, iris_y, iris_r)
+    write(iris_showed, "iris_showed.png")
 
     # preparing the result
     draw_circle(result, pupil_x, pupil_y, pupil_r)
     draw_circle(result, iris_x, iris_y, iris_r)
     compare(result, "==", 255, result, original_image, result)
+    write(result, "result.png")
     result.show()
 
 if __name__ == "__main__":
